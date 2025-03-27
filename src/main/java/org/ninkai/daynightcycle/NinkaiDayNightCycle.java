645f76ff8852e208f7a9006edc9a81bdd5293f83
poulpy2k <contact@jeremy-laurent.com>
2025-03-27 22:22:58 +0100

    Initial commit 🚀

---
+++ src/main/java/org/ninkai/daynightcycle/NinkaiDayNightCycle.java
@@ -0,0 +1,17 @@
+package org.ninkai.daynightcycle;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NinkaiDayNightCycle extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
